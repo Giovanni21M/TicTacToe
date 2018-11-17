d66ec3f3a6d0df034326a9fd800a50ae225479d2
Giovanni21M <giovanni21martinus@gmail.com>
2018-11-17 12:08:55 -0500

    Create Score class with an array of arrays (winning positions in the game. Work towards calling class Score's method in onClick function.

--- app/src/main/java/com/giovannimartinus/tictactoe/MainActivity.java
+++ app/src/main/java/com/giovannimartinus/tictactoe/MainActivity.java
@@ -8,9 +8,43 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
+    // array with an item of 2 (means unplayed) for each (x,y) on the grid
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    // will use to determine whether the game is active or not
+    boolean gameIsActive = true;
+
+
+    class Score {
+
+        // an array of arrays
+        int[][] winningPositions = {
+                {0,1,2}, {3,4,5},
+                {6,7,8}, {0,3,6},
+                {1,4,7}, {2,5,8},
+                {0,4,8}, {2,4,6}
+        };
+
+        public void determineScore(int x)
+
+    }
+
+
+    //  onClick method for grid layout spaces
     public void markSpace(View view) {
 
+        //create a variable of ImageView type
         ImageView marker = (ImageView) view;
+
+        // assign a class to an instant
+        Score findScore = new Score();
+
+        // get the tag of marker and assign to variable of integer type
+        int selectedMarker = Integer.parseInt(marker.getTag().toString());
+
+        if (gameState[selectedMarker] == 2 && gameIsActive) {
+
+        }
 
         marker.setTranslationY(-1000f);
 
@@ -23,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 .setDuration(1000);
 
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
